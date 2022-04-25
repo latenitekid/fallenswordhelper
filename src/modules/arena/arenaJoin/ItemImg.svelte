97532881a6fc2b485b7213b320ae17786c2250ec
Adrian Tompkins <atompkins@users.noreply.github.com>
2022-04-25 03:21:15 +0100

    arena join items (#364)

--- src/modules/arena/arenaJoin/ItemImg.svelte
+++ src/modules/arena/arenaJoin/ItemImg.svelte
@@ -2,14 +2,16 @@
   import { cdn } from '../../system/system';
   import playerId from '../../common/playerId';
 
+  let className = '';
+
   export let item = 0;
-  export let lrg = 0;
+  export { className as class };
 
   const src = (i) => `${cdn}items/${i.b}.gif`;
   const tipped = (i) => `fetchitem.php?item_id=${i.b}&inv_id=${i.a}&t=1&p=${playerId()}`;
 </script>
 
-<div class:lrg>
+<div class="{className}">
   {#if item}
     <img alt="{item.name}" src="{src(item)}" class="tip-dynamic" data-tipped="{tipped(item)}">
   {/if}
@@ -21,7 +23,16 @@
     background-image: url(https://cdn2.fallensword.com/ui/inventory/2x3.png);
     display: flex;
   }
+  .sml {
+    min-height: 30px;
+    min-width: 30px;
+  }
+  .med {
+    min-height: 60px;
+    min-width: 60px;
+  }
   .lrg {
-    height: 90px;
+    min-height: 90px;
+    min-width: 60px;
   }
 </style>
