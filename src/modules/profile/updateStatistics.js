e383a1c88522f20942186e28a95cef1a371679fe
Adrian Tompkins <atompkins@users.noreply.github.com>
2022-08-04 12:09:24 +0100

    profile-updateStats (#432)
    * profile-updateStats
    
    * codebeat

--- src/modules/profile/updateStatistics.js
+++ src/modules/profile/updateStatistics.js
@@ -1,25 +1,50 @@
-import getArrayByTagName from '../common/getArrayByTagName';
-import getElementById from '../common/getElementById';
+import createStyle from '../common/cElement/createStyle';
 import getElementsByTagName from '../common/getElementsByTagName';
-import getText from '../common/getText';
+import getTextTrim from '../common/getTextTrim';
+import insertElementBefore from '../common/insertElementBefore';
+import querySelector from '../common/querySelector';
+import querySelectorArray from '../common/querySelectorArray';
 import trim from '../common/trim';
 import setInnerHtml from '../dom/setInnerHtml';
 import { defTable } from '../support/constants';
 
-function removeStatTable(el) {
-  const tde = getElementsByTagName('td', el);
+const statStyle = `.profile-stat-bonus { font-size: x-small; }
+.fshCharStats { table-layout: fixed; }
+.fshCharStats td:first-of-type { width: 22%; }
+.fshCharStats td:nth-of-type(2) { width: 25%; }
+.fshCharStats td:nth-of-type(3) { width: 18%; }
+.fshCharStats td:nth-of-type(4) { width: 35%; }`;
+
+function statBonus(td) {
+  const tdText = getTextTrim(td);
+  return tdText ? ` <div class="profile-stat-bonus">${tdText}</div>` : '';
+}
+
+function removeStatTable(tbl) {
+  const [td1, td2] = getElementsByTagName('td', tbl);
   setInnerHtml(
-    `<span id="${tde[0].id}">${
-      trim(tde[0].innerHTML.replace(/&nbsp;/g, ' '))}</span> <div class="profile-stat-bonus">${
-      getText(tde[1])}</div>`,
-    el.parentNode,
+    `<span id="${td1.id}">${trim(td1.innerHTML.replace(/&nbsp;/g, ' '))}</span>${statBonus(td2)}`,
+    tbl.parentNode,
   );
 }
 
+const removeAttr = (root, el, attr) => querySelectorArray(`${el}[${attr}]`, root)
+  .forEach((l) => l.removeAttribute(attr));
+
+function cleanTable(charStats) {
+  removeAttr(charStats, 'tr', 'style');
+  removeAttr(charStats, 'td', 'width');
+  querySelector('td[colspan]', charStats)?.removeAttribute('colspan');
+}
+
+function updateCharStats(charStats) {
+  charStats.classList.add('fshCharStats');
+  cleanTable(charStats);
+  querySelectorArray(defTable, charStats).forEach(removeStatTable);
+  insertElementBefore(createStyle(statStyle), charStats);
+}
+
 export default function updateStatistics() {
-  const pLC = getElementById('profileLeftColumn');
-  if (pLC) {
-    const charStats = getElementsByTagName(defTable, pLC)[0];
-    getArrayByTagName(defTable, charStats).forEach(removeStatTable);
-  }
+  const charStats = querySelector('#profileLeftColumn table');
+  if (charStats) updateCharStats(charStats);
 }
