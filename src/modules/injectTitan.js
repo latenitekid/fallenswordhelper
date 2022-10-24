8e365bfdd0ce3e020422084c31c5e11e94ffcc2b
Adrian Tompkins <atompkins@users.noreply.github.com>
2022-10-24 23:58:20 +0100

    better scout tower link in titan log (#468)

--- src/modules/injectTitan.js
+++ src/modules/injectTitan.js
@@ -3,12 +3,8 @@ import { scouttowerUrl } from './support/constants';
 import { pcc } from './support/layout';
 
 export default function injectTitan() {
-  const titanTable = pcc().children[0];
-  let newRow = titanTable.insertRow(2);
-  insertHtmlBeforeEnd(newRow, '<br>');
-  newRow = titanTable.insertRow(3);
   insertHtmlBeforeEnd(
-    newRow,
-    `<td class="fshCenter fshBold">[ <a href="${scouttowerUrl}">Scout Tower</a> ]</td>`,
+    pcc().children[0].rows[2].cells[0],
+    `<div class="fshBold fshCenter">[ <a href="${scouttowerUrl}">Scout Tower</a> ]</div>`,
   );
 }
