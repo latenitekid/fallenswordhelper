beda3380347530a7e491085561acd6fa64100885
Adrian Tompkins <atompkins@users.noreply.github.com>
2022-11-10 10:43:37 +0000

    titan-scout-event (#472)

--- src/modules/injectTitan.js
+++ src/modules/injectTitan.js
@@ -1,10 +1,17 @@
-import insertHtmlBeforeEnd from './common/insertHtmlBeforeEnd';
+import sendEvent from './analytics/sendEvent';
+import createAnchor from './common/cElement/createAnchor';
+import createDiv from './common/cElement/createDiv';
+import insertElement from './common/insertElement';
+import insertTextBeforeEnd from './common/insertTextBeforeEnd';
+import onclick from './common/onclick';
 import { scouttowerUrl } from './support/constants';
 import { pcc } from './support/layout';
 
 export default function injectTitan() {
-  insertHtmlBeforeEnd(
-    pcc().children[0].rows[2].cells[0],
-    `<div class="fshBold fshCenter">[ <a href="${scouttowerUrl}">Scout Tower</a> ]</div>`,
-  );
+  const aLink = createAnchor({ href: scouttowerUrl, textContent: 'Scout Tower' });
+  onclick(aLink, () => sendEvent('titan', 'scout tower'));
+  const cDiv = createDiv({ className: 'fshBold fshCenter', textContent: '[ ' });
+  insertElement(cDiv, aLink);
+  insertTextBeforeEnd(cDiv, ' ]');
+  insertElement(pcc().children[0].rows[2].cells[0], cDiv);
 }
