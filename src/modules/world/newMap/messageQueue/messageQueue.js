60354cd44c66638f36ce63061683b4cb7c31ba31
Adrian Tompkins <atompkins@users.noreply.github.com>
2023-01-08 23:59:33 +0000

    message-stack-pref (#501)

--- src/modules/world/newMap/messageQueue/messageQueue.js
+++ src/modules/world/newMap/messageQueue/messageQueue.js
@@ -1,8 +1,9 @@
 import getElementById from '../../../common/getElementById';
+import getValue from '../../../system/getValue';
 import MessageQueue from './MessageQueue.svelte';
 
 const startApp = (target) => new MessageQueue({ target });
 
 export default function messageQueue() {
-  startApp(getElementById('messageCenter'));
+  if (getValue('messageStack')) startApp(getElementById('messageCenter'));
 }
